dbe6c6e622fd4f69061ec9fe284865719538cdad
CressChe <84721195+CressChe@users.noreply.github.com>
2021-05-24 18:43:00 +0100

    Add files via upload
    FizzBuzzFezzBang

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,2 +1,31 @@
-for (let j = 1 ; j < 101 ; j++)
-console.log(j)
+for (let i = 1 ; i < 1001 ; i++)
+let newArr = []
+if (i % 3 == 0) {
+    newArr.push('Fizz');
+} 
+if (i % 13 == 0) {
+    newArr.push('Fezz')
+}
+if (i % 5 == 0) {
+    newArr.push('Buzz');
+}
+if (i % 7 == 0) {
+    newArr.push('Bang');
+}
+if (i % 11 == 0) { 
+    newArr.push('Bong');
+}
+if (i % 17 == 0) {
+    newArr.reverse()
+}
+if (newArr.length == 0) {
+    console.log(i);
+}
+else if (i % 13 == 0 && i % 11 == 0) {
+    console.log('FezzBong');
+}
+else if (i % 11 == 0) {
+    console.log('Bong');
+}
+else {console.log(newArr.join(''));
+}
